b1f0f19497bbf2e9ff9099501cafafc2513cbe82
Benny <benny.think@gmail.com>
2017-03-30 22:12:08 +0800

    格式

--- header.php
+++ header.php
@@ -1,3 +1,6 @@
+<!--
+Some of us get dipped in flat, some in satin, some in gloss. But every once in a while you find someone who's iridescent, and when you do, nothing will ever compare.
+-->
 <!doctype html>
 <html lang="zh-CN">
 <head>
