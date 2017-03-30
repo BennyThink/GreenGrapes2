702aacc8bc32434fafc09e10b233c7d10f281e54
Benny <benny.think@gmail.com>
2017-03-30 13:09:10 +0800

    404居中

--- 404.php
+++ 404.php
@@ -1,6 +1,18 @@
 <?php $this->need('header.php'); ?>
+<style>
+	body {
+	font: 14px/1 "Helvetica Neue",Helvetica,Arial,sans-serif;
+	margin:0;
+	padding:0;
+	background: #DFF4FF;
+	}
 
-    <div class="container">
+	.wrapper {
+	
+	text-align: center;
+	}
+</style>
+    <div class="wrapper">
         <div class="error-page">
             <h2 class="post-title">404 - <?php _e('页面没找到'); ?></h2>
             <p><?php _e('哎呦卧槽~404了！搜索试试？或者帮我喂喂可怜的小仓鼠吧！ '); ?></p>
