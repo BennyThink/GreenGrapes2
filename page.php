cf39e283b92ed97074737c12d6d21c0e36aee4cc
Benny <benny.think@gmail.com>
2017-03-30 18:20:47 +0800

    浏览量、字数统计

--- page.php
+++ page.php
@@ -9,7 +9,12 @@
                         <span class="label label-green"><i class="fa fa-user"></i> <a href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></span>
                         <span class="label label-green"><i class="fa fa-tags"></i> <?php $this->category(','); ?></span>
                         <span class="label label-green"><i class="fa fa-calendar"></i> <?php $this->date('Y-m-d'); ?></span>
-<span class="label label-green"><i class="fa fa-visit"></i> <?php _e('浏览量: '); ?><?php $this->views(); ?></span>
+<span class="label label-green"><?php if($this->user->hasLogin()):?><i class="fa fa-edit"></i><a href="<?php $this->options->adminUrl(); ?>write-post.php?cid=<?php echo $this->cid;?>" >编辑</a><?php endif;?></span>
+
+<span class="label label-green"><i class="fa fa-visit"></i> <?php _e('浏览量: '); ?><?php get_post_view($this) ?></span>
+<span class="label label-green"><i class="fa fa-count"></i> <?php _e('字数统计: '); ?><?php echo art_count($this->cid); ?></span>
+<span class="label label-green"><i class="fa fa-lasst"></i> <?php _e('最后更新： '); ?><?php echo gmdate('Y-m-d H:i', $this->modified + Typecho_Widget::widget('Widget_Options')->timezone); ?></span>
+
                     </p>
                 </div>
                 <div class="article-content clearfix">
