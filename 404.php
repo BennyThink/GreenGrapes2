70a4f6d80a9a41beee32f7460ce145defaffa1d3
Benny <benny.think@gmail.com>
2017-03-30 13:06:12 +0800

    404

--- 404.php
+++ 404.php
@@ -3,7 +3,8 @@
     <div class="container">
         <div class="error-page">
             <h2 class="post-title">404 - <?php _e('页面没找到'); ?></h2>
-            <p><?php _e('你想查看的页面已被转移或删除了, 要不要搜索看看: '); ?></p>
+            <p><?php _e('哎呦卧槽~404了！搜索试试？或者帮我喂喂可怜的小仓鼠吧！ '); ?></p>
+<br><embed width="290" height="230" src="https://o51bfbumd.qnssl.com/h5hamster.html" />
             <form method="post">
                 <p><input type="text" name="s" class="text" autofocus /></p>
                 <p><button type="submit" class="submit"><?php _e('搜索'); ?></button></p>
