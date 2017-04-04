7470c84255434fe97861bd048cf841f819ff2c70
Benny <benny.think@gmail.com>
2017-04-04 17:34:52 +0800

    现在评论区可以显示UA了！

--- functions.php
+++ functions.php
@@ -154,7 +154,99 @@ else{
 
 }
 
+function getUA($ua){
+	 
+        //开始解析操作系统
+        $os = null;
+        if (preg_match('/Windows NT 6.0/i', $ua)) 
+            $os = "Windows Vista";
+        elseif (preg_match('/Windows NT 6.1/i', $ua)) 
+            $os = "Windows 7";
+        elseif (preg_match('/Windows NT 6.2/i', $ua)) 
+            $os = "Windows 8";
+        elseif (preg_match('/Windows NT 6.3/i', $ua)) 
+            $os = "Windows 8.1";
+        elseif (preg_match('/Windows NT 10.0/i', $ua)) 
+            $os = "Windows 10";
+        elseif (preg_match('/Windows NT 5.1/i', $ua)) 
+            $os = "Windows XP";
+        elseif (preg_match('/Windows NT 5.2/i', $ua) && preg_match('/Win64/i', $ua)) 
+            $os = "Windows XP 64 bit";
+		elseif (preg_match('/Windows NT 5.0/i', $ua)) 
+            $os = "Windows 2000 Professional";
+        elseif (preg_match('/Android ([0-9.]+)/i', $ua, $matches)) 
+            $os = "Android " . $matches[1];
+        elseif (preg_match('/iPhone OS ([_0-9]+)/i', $ua, $matches)) 
+            $os = 'iPhone ' . $matches[1];
+		elseif (preg_match('/iPad/i', $ua)) 
+            $os = "iPad";
+		elseif (preg_match('/Mac OS X ([_0-9]+)/i', $ua, $matches)) 
+            $os = 'Mac OS X ' . $matches[1];		
+		elseif (preg_match('/Windows Phone ([_0-9]+)/i', $ua, $matches)) 
+            $os = 'Windows Phone ' . $matches[1];
+		elseif(preg_match('/Gentoo/i',$ua))
+			$os = 'Gentoo Linux';
+		elseif(preg_match('/Ubuntu/i',$ua))
+			$os = 'Ubuntu Linux';
+		elseif(preg_match('/Debian/i',$ua))
+			$os = 'Debian Linux';
+		elseif(preg_match('/curl/i',$ua))
+			$os = 'Linux distribution';
+		elseif(preg_match('/X11; FreeBSD/i',$ua))
+			$os = 'FreeBSD';
+		elseif(preg_match('/X11; Linux/i',$ua))
+			$os = 'Linux';
+		elseif(preg_match('/X11; SunOS/i',$ua))
+			$os = 'SunOS';
+        else 
+            $os = '未知操作系统';
+        
+		//解析浏览器
+        if (preg_match('#(Camino|Chimera)[ /]([a-zA-Z0-9.]+)#i', $ua, $matches)) 
+            $browser = 'Camino ' . $matches[2];
+        elseif (preg_match('#SE 2([a-zA-Z0-9.]+)#i', $ua, $matches)) 
+            $browser = '搜狗浏览器 2' . $matches[1];
+        elseif (preg_match('#360([a-zA-Z0-9.]+)#i', $ua, $matches)) 
+            $browser = '360浏览器 ' . $matches[1];
+        elseif (preg_match('#Maxthon( |\/)([a-zA-Z0-9.]+)#i', $ua, $matches)) 
+			$browser = 'Maxthon ' . $matches[2];
+		elseif (preg_match('#Edge( |\/)([a-zA-Z0-9.]+)#i', $ua, $matches)) 
+            $browser = 'Edge ' . $matches[2];
+		elseif (preg_match('#MicroMessenger/([a-zA-Z0-9.]+)#i', $ua, $matches) || preg_match('#Mobile MQQBrowser#i', $ua, $matches)) 
+            $browser = '手机QQ 或 微信  ' . $matches[1];	
+		elseif (preg_match('#Chrome/([a-zA-Z0-9.]+)#i', $ua, $matches)) 
+            $browser = 'Chrome ' . $matches[1];
+        elseif (preg_match('#XiaoMi/MiuiBrowser/([0-9.]+)#i', $ua, $matches)) 
+            $browser = '小米浏览器 ' . $matches[1];
+        elseif (preg_match('#Safari/([a-zA-Z0-9.]+)#i', $ua, $matches)) 
+            $browser = 'Safari ' . $matches[1];
+        elseif (preg_match('#opera mini#i', $ua)) {
+            preg_match('#Opera/([a-zA-Z0-9.]+)#i', $ua, $matches);
+            $browser = 'Opera Mini ' . $matches[1];}
+        elseif (preg_match('#Opera.([a-zA-Z0-9.]+)#i', $ua, $matches)) 
+            $browser = 'Opera ' . $matches[1];
+        elseif (preg_match('#TencentTraveler ([a-zA-Z0-9.]+)#i', $ua, $matches)) 
+            $browser = '腾讯TT浏览器 ' . $matches[1];
+        elseif (preg_match('#UCWEB([a-zA-Z0-9.]+)#i', $ua, $matches)) 
+            $browser = 'UCWEB ' . $matches[1];
+        elseif (preg_match('#wp-(iphone|android)/([a-zA-Z0-9.]+)#i', $ua, $matches)) 
+            $browser = 'WordPress客户端 ' . $matches[1];
+        elseif (preg_match('#MSIE ([a-zA-Z0-9.]+)#i', $ua, $matches)) 
+            $browser = 'Internet Explorer ' . $matches[1];         
+		elseif (preg_match('#Trident/([a-zA-Z0-9.]+)#i', $ua, $matches)) 
+            $browser = 'Internet Explorer 11' ;
+		elseif (preg_match('#Outlook Mail ([a-zA-Z0-9.]+)#i', $ua, $matches)) 
+            $browser = 'Windows 10 邮件应用'. $matches[1] ;       		
+		elseif (preg_match('#(Firefox|Phoenix|Firebird|BonEcho|GranParadiso|Minefield|Iceweasel)/([a-zA-Z0-9.]+)#i', $ua, $matches)) 
+            $browser = 'Firefox ' . $matches[2];
+        elseif(preg_match('/curl/i',$ua))
+			$browser = 'curl';
+		else 
+            $browser = '未知浏览器';	
+		
+        return $os . "  |  " . $browser;
 
+}
 
 
 /**
@@ -259,6 +351,8 @@ echo $commentClass;
                 //$comments->date($singleCommentOptions->dateFormat);Y-m-d H:i:s
 				$comments->date('Y-m-d H:i:s');
                     $singleCommentOptions->afterDate(); ?></time>
+					<!--UA加在这里-->
+					<?php echo '<font  color=#ff6600>'.getUA($comments->agent).'</font>'; ?>
             <?php $comments->reply($singleCommentOptions->replyWord); ?>
         </div>
     </div>
