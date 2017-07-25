5decd9e35f9b0de8791eb62451a4d8db02730096
Benny <benny.think@gmail.com>
2017-07-25 12:50:05 +0800

    树莓派也算移动设备了

--- functions.php
+++ functions.php
@@ -486,13 +486,14 @@ function isMobile() {
 	if ( empty($_SERVER['HTTP_USER_AGENT']) ) {
 		$is_mobile = false;
 	} elseif ( strpos($_SERVER['HTTP_USER_AGENT'], 'Mobile') !== false // many mobile devices (all iPhone, iPad, etc.)
-		|| strpos($_SERVER['HTTP_USER_AGENT'], 'Android') !== false
-		|| strpos($_SERVER['HTTP_USER_AGENT'], 'Silk/') !== false
-		|| strpos($_SERVER['HTTP_USER_AGENT'], 'Kindle') !== false
-		|| strpos($_SERVER['HTTP_USER_AGENT'], 'BlackBerry') !== false
-		|| strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== false
-		|| strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mobi') !== false ) {
-			$is_mobile = true;
+	           || strpos($_SERVER['HTTP_USER_AGENT'], 'Android') !== false
+	           || strpos($_SERVER['HTTP_USER_AGENT'], 'Silk/') !== false
+	           || strpos($_SERVER['HTTP_USER_AGENT'], 'Kindle') !== false
+	           || strpos($_SERVER['HTTP_USER_AGENT'], 'arm') !== false
+	           || strpos($_SERVER['HTTP_USER_AGENT'], 'BlackBerry') !== false
+	           || strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== false
+	           || strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mobi') !== false ) {
+		$is_mobile = true;
 	} else {
 		$is_mobile = false;
 	}
